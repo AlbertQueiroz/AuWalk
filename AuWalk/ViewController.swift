374d5d554de1eb3a987ac3f2e6686c7fcdd45965
Albert Rayneer <rayneerqueiroz@gmail.com>
2020-06-09 10:04:05 -0300

    Initial Commit

---
+++ AuWalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuWalk
+//
+//  Created by Albert Rayneer on 09/06/20.
+//  Copyright © 2020 AuWalk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
