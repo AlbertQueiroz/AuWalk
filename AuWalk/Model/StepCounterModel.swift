94ee8d291443fd3c623608d45535cdd6648c6de2
talesconrado <talesmateuscc@gmail.com>
2020-06-18 18:42:22 -0300

    adds feature getting today steps

--- AuWalk/Model/StepCounterModel.swift
+++ AuWalk/Model/StepCounterModel.swift
@@ -11,23 +11,21 @@ import HealthKit
 class StepCounterModel {
     
     var healthStore: HKHealthStore?
-
     
     init() {
-        if HKHealthStore.isHealthDataAvailable() {
-            print("HKStore is available.")
-            healthStore = HKHealthStore()
-        } else {
+        if !HKHealthStore.isHealthDataAvailable() {
             print("HKStore isn't available.")
+        } else {
+            healthStore = HKHealthStore()
         }
-        
+
         checkHKAvailability()
+        getTodaySteps(completion: printData(data:))
         
     }
     
-    func retrieveStepCount() {
-        
-        
+    func printData(data: Double) {
+        print("Passos dados: \(data)")
     }
     
     func checkHKAvailability() {
@@ -38,6 +36,29 @@ class StepCounterModel {
                 print("Reading step data was not allowed.")
             }
         }
+    }
+    
+    
+    func getTodaySteps(completion: @escaping (Double) -> Void) {
+        
+        let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)
+        
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+        
+        let query = HKStatisticsQuery(quantityType: stepsQuantityType!, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, _) in
+            
+            guard let result = result, let sum = result.sumQuantity() else {
+                completion(0.0)
+                return
+            }
+        
+            completion(sum.doubleValue(for: HKUnit.count()))
+        }
+        
+        healthStore?.execute(query)
+        
     }
     
 }
